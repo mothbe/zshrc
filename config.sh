911ba467d607f70256b8b0a3e129c3e63ea5132b
Marcin Witowski <marcin.witowski@void.pl>
2020-05-03 15:18:57 +0200

    removing not necessary code

--- config.sh
+++ config.sh
@@ -1,18 +1,6 @@
 ZSH_COMPDUMP=${HOME}/.zcompdump
 autoload -Uz compinit
-if [ ! -f ${ZSH_COMPDUMP} ]; then
-  echo "File not exists ${ZSH_COMPDUMP}"
-  compinit -d "${ZSH_COMPDUMP}"
-fi
-
-
-if [ $(date +'%j') != $(stat -f '%Sm' -t '%j' ${ZSH_COMPDUMP}) ]; then
-  echo "Old data in ${ZSH_COMPDUMP}"
-  rm -f "${ZSH_COMPDUMP}"
-  compinit -d "${ZSH_COMPDUMP}"
-else
-  compinit -u -C -d "${ZSH_COMPDUMP}"
-fi
+compinit -d "${ZSH_COMPDUMP}"
 
 # Custom PATH for MacOS
 if [[ "$OSTYPE" == (darwin)* ]]; then
