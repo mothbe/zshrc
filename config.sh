1462975b45add24819a9f71f8733188e0c3fd314
Marcin Witowski <marcin.witowski@void.pl>
2020-05-29 01:33:28 +0200

    disable path to Python 2

--- config.sh
+++ config.sh
@@ -4,5 +4,5 @@ compinit -d "${ZSH_COMPDUMP}"
 
 # Custom PATH for MacOS
 if [[ "$OSTYPE" == (darwin)* ]]; then
-  export PATH=$PATH:/Users/mwitowski/Library/Python/2.7/bin/:/Users/mwitowski/Library/Python/2.7/bin
+  export PATH=$PATH:/Users/marcin/Library/Python/3.7/bin
 fi
