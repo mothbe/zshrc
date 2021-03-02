60a21f8d365792a9dd289e9e0331c092216820e0
Marcin Witowski <mwitowski@redlock.pl>
2021-03-02 17:01:46 +0100

    fix path env variable

--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
 ZSH_COMPDUMP=${HOME}/.zcompdump
 autoload -Uz compinit
-compinit -d "${ZSH_COMPDUMP}"
+compinit -u -d "${ZSH_COMPDUMP}"
 
 # Custom PATH for MacOS
 if [[ "$OSTYPE" == (darwin)* ]]; then
-  export PATH=$PATH:/Users/marcin/Library/Python/3.7/bin
+  export PATH=$PATH:$HOME/Library/Python/3.7/bin
 fi
